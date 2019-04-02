7a824472263e679722534fb086cf4230d0918fdb
Artem Yurchenko <artemyurchenko@zoho.com>
2019-04-03 00:15:52 +0300

    add docs

--- ld_context.h
+++ ld_context.h
@@ -10,19 +10,51 @@
 extern "C" {
 #endif
 
+/**
+ *  \brief Stores search progress, lookup options and atomic flag to interrupt
+ *  search.
+ *
+ *  Must have fields `dups_list`, `cancelled`, `min_file_size`. All field must
+ *  be accessed only after initialisation via `ld_context_init`.
+ *
+ *  \var struct ld_ranked_list dups_list \brief List of duplicates found at the
+ *  previous call of `ld_next_duplicate`. Undefined, if `ld_next_duplicate`
+ *  hasn't been called or was unsuccessful. If defined, ownership should be
+ *  taken by someone, `ld_context_clear` won't free the list.
+ *
+ *  \var atomic_bool const *cancelled
+ *  \brief Flag to indicate if search should interrupt
+ *
+ *  \var size_t min_file_size
+ *  \brief Files with size in bytes less than this will * be ignored. Default: 16
+ *
+ */
 struct ld_context {
     char *dirname;                                // owns
     struct ld_duplicates_tree duplicates_tree;    // owns
     struct ld_duplicates_tree_node *current_node; // owned by duplicates_tree
     struct ld_ranked_list dups_list;              // doesn't own
-    atomic_bool const *cancelled;                 // doesn't own
+
+    atomic_bool const *cancelled;
 
     // options
-    size_t min_file_size; // in bytes
+    size_t min_file_size;
 };
 
+/**
+ *  \brief initialises `ld_context`
+ *
+ *  Sets default option values and directory to search for duplicates in. Must
+ *  be accompanied by `ld_context_free`.
+ *
+ *  \param dirname directory to search for duplicates in
+ *  \return `ld_ERR_OK` if succeeded. Otherwise, proper error code.
+ */
 struct ld_error ld_context_init(struct ld_context *, char const *dirname);
 
+/**
+ * \brief cleans up resources owned by `ld_context`.
+ */
 void ld_context_clear(struct ld_context *);
 
 #ifdef __cplusplus
--- ldupes.h
+++ ldupes.h
@@ -7,6 +7,22 @@
 extern "C" {
 #endif
 
+/**
+ * @brief ld_next_duplicate Find next group of duplicates and store results in
+ * \param context.
+ *
+ * Each duplicate size is less than size of that in the previous call.
+ *
+ * @param context
+ * @return ld_ERR_OK if next duplicate group is successfully found;
+ * ld_ERR_END_OF_ITERATION if there are no more duplicates;
+ * ld_ERR_CANCELLED if file gathering was cancelled from another thread;
+ * ld_ERR_NULL if context is null;
+ * ld_ERR_CANT_ACCESS if some error occurred when working with files, in this case ld_error.message is set to the name of the said file;
+ * ld_ERR_NOT_DIRECTORY if context.dirname is not in fact a directory;
+ * ld_ERR_OUT_OF_MEMORY;
+ * ld_ERR_HASHING_ERROR;
+ */
 struct ld_error ld_next_duplicate(struct ld_context *context);
 
 #ifdef __cplusplus
