a860bb739bce37437d550eed16958200841bd439
RubyUmbra <rubyumbra@gmail.com>
2019-04-29 00:14:46 +0300

    add static lib

---
+++ greater.c
@@ -0,0 +1,5 @@
+#include "greater.h"
+
+#include <stdio.h>
+
+void printHelloWorld(void) { printf("Hello, world!\n"); }
---
+++ greater.h
@@ -0,0 +1,6 @@
+#ifndef OS_LIB_GREATER_H
+#define OS_LIB_GREATER_H
+
+void printHelloWorld(void);
+
+#endif //OS_LIB_GREATER_H
