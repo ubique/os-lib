8db65f782abc744f404827b6dbba56022413f013
RubyUmbra <rubyumbra@gmail.com>
2019-04-29 00:16:05 +0300

    add dynamic lib

---
+++ usergreater.c
@@ -0,0 +1,5 @@
+#include "usergreater.h"
+
+#include <stdio.h>
+
+void printHelloUserString(const char* username) { printf("Hello, %s!\n", username); }
---
+++ usergreater.h
@@ -0,0 +1,6 @@
+#ifndef OS_LIB_USERGREATER_H
+#define OS_LIB_USERGREATER_H
+
+void printHelloUserString(const char* username);
+
+#endif //OS_LIB_USERGREATER_H
