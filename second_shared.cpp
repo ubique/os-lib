3efbd87cb9a8329c83c309d3dd085f09599a3c9f
dumpling <valeratep@mail.ru>
2019-04-30 19:31:58 +0300

    Minimal solution

---
+++ second_shared.cpp
@@ -0,0 +1,9 @@
+//
+// Created by dumpling on 30.04.19.
+//
+
+#include <string>
+
+extern "C" std::string get_exclamation() {
+    return "!";
+}
---
+++ shared.cpp
@@ -0,0 +1,9 @@
+//
+// Created by dumpling on 30.04.19.
+//
+
+#include <string>
+
+std::string get_world() {
+    return "world";
+}
