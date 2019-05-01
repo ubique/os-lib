cc863e978377c91ced7a4bf8c1131b32bac41b12
doreshnikov <dan.io.oreshnikov@gmail.com>
2019-05-01 21:20:49 +0300

    First modification: ~ readme corrected + added lib/ + added three libraries + testes all libs

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include "lib/static.hpp"
+#include "lib/dynamic.hpp"
+#include "lib/dynamic_linkage.hpp"
+
+int main() {
+
+    print_ascii_number(729710);
+    print_random_name();
+    print_inversion(2);
+
+}
