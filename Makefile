1722dbd14ad3430c779326d7a208ee6eef8c4549
doreshnikov <dan.io.oreshnikov@gmail.com>
2019-05-01 22:38:56 +0300

    small fixes

--- Makefile
+++ Makefile
@@ -6,12 +6,12 @@ CXX=g++ -std=c++14
 all: main
 
 clean:
-	rm main *.o
+	rm main *.o lib/*
 
 # executable
 
-main: main.o static.a dynamic_linkage.so dynamic_load.so
-	$(CXX) $< -L. -l:./$(OUT_PATH)/static.a -l:./$(OUT_PATH)/dynamic_linkage.so -Wl,-rpath,. -ldl -o $@
+main: main.o $(OUT_PATH)/static.a $(OUT_PATH)/dynamic_linkage.so $(OUT_PATH)/dynamic_load.so
+	$(CXX) -L./$(OUT_PATH) -Wl,-rpath,. -o $@ $^ -ldl
 
 main.o: main.cpp
 	$(CXX) -c $^ -o $@
@@ -21,21 +21,21 @@ main.o: main.cpp
 static.o: $(LIB_PATH)/static.cpp $(LIB_PATH)/static.hpp
 	$(CXX) -c $< -o $@
 
-static.a: static.o
-	ar rcs $(OUT_PATH)/$@ $<
+$(OUT_PATH)/static.a: static.o
+	ar rcs $@ $<
 
 # dynamic linking library
 
 dynamic_linkage.o: $(LIB_PATH)/dynamic_linkage.cpp $(LIB_PATH)/dynamic_linkage.hpp
 	$(CXX) -fPIC -c $< -o $@
 
-dynamic_linkage.so: dynamic_linkage.o
-	$(CXX) -shared $< -o $(OUT_PATH)/$@
+$(OUT_PATH)/dynamic_linkage.so: dynamic_linkage.o
+	$(CXX) -shared $< -o $@
 
-# dynamic load librari
+# dynamic load library
 
 dynamic_load.o: $(LIB_PATH)/dynamic_load.cpp $(LIB_PATH)/dynamic_load.hpp
 	$(CXX) -fPIC -c $< -o $@
 
-dynamic_load.so: dynamic_load.o
-	$(CXX) -shared $< -o $(OUT_PATH)/$@
+$(OUT_PATH)/dynamic_load.so: dynamic_load.o
+	$(CXX) -shared $< -o $@
--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 #include "src/static.hpp"
 #include "src/dynamic_linkage.hpp"
+#include "src/dynamic_load.hpp"
 
 #include <iostream>
 #include <string>
 #include <cstring>
 
-#include <unistd.h>
 #include <dlfcn.h>
 
 namespace console {
@@ -19,6 +19,11 @@ namespace console {
             std::cerr << std::strerror(errno);
         }
         std::cerr << std::endl << _DEFAULT;
+        return 0;
+    }
+
+    int report(std::string const &message, std::string const &error) {
+        std::cerr << _ERROR << message << " " << error << std::endl << _DEFAULT;
         return 0;
     }
 
@@ -37,12 +42,12 @@ int main() {
     void *lib = dlopen(library_name.c_str(), RTLD_LAZY);
 
     if (lib == nullptr) {
-        return console::report("Can not link with external library:", errno);
+        return console::report("Can not link with external library:", dlerror());
     }
-    auto print_random_name = (void(*)()) dlsym(lib, "print_random_name");
+    auto print_random_name = (void (*)()) dlsym(lib, "print_random_name");
     if (print_random_name == nullptr) {
         dlclose(lib);
-        return console::report("Can not load function from linked library:", errno);
+        return console::report("Can not load function from linked library:", dlerror());
     }
 
     (*print_random_name)();
